aca50399aef7afff7244ce7698e89d1b369087e8
abelgarcia4545 <149451272+abelgarcia4545@users.noreply.github.com>
2023-11-06 19:43:11 -0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     merge_str = sub2 + ' ' + sub1
     print(sub2)
     print(sub1)
-    print(merged_str)
+    print(merge_str)
 
     #########################################
     # Do not delete the return statement
